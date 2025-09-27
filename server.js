ddf8c9d438db2e598394cd017d8803b86ad7b888
devireddyraj <91554387+devireddyraj@users.noreply.github.com>
2025-09-27 21:00:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "https://68d7fbd9cf952c5375a6608f--calm-fenglisu-236a15.netlify.app/", // change later to your frontend URL
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("IRT Server is running");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("call-irt", (data) => {
+    io.emit("irt-notification", data);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
