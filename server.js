669cb3d86d3f095242dc7e4df220b34d9664120f
devireddyraj <91554387+devireddyraj@users.noreply.github.com>
2025-11-18 14:30:25 +0530

    Update CORS origin for server configuration

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://68d80922cf952c8271a65f21--beamish-dusk-6440e1.netlify.app/", // change later to your frontend URL
+    origin: "https://irt-notification.netlify.app/", // change later to your frontend URL
     methods: ["GET", "POST"]
   }
 });
